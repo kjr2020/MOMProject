a64d84498b55481a45509f8c74edd78eb702adf5
kjr2020 <kjr202020@gmail.com>
2021-04-15 16:58:25 +0900

    Complete KafkaConsumer Code

--- MyKafkaConsumer/src/main/java/org/idpl/mju/mykafkaconsumer/MyConsumer.java
+++ MyKafkaConsumer/src/main/java/org/idpl/mju/mykafkaconsumer/MyConsumer.java
@@ -11,51 +11,60 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
 
 public class MyConsumer {
-
+	static final String BOOTSTRAP_SERVER = "master:9092";
+	static final String TOPIC_NAME = "idpl-topic";
+	static final String GROUP_ID = "idpl-consumer";
+	static final String ENABLE_AUTO_COMMIT = "true";
+	static final String AUTO_OFFSET_RESET = "latest";
+	static final String KEY_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer";
+	static final String VALUE_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer";
+	static final String RESULT_FILE_NAME = "KafkaResult/KafkaConsumer-";
+	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "master:9092");
-		props.put("group.id", "idpl-consumer");
-		props.put("enable.auto.commit", "true");
-		props.put("auto.offset.reset", "latest");
-		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		
+		props.put("bootstrap.servers", BOOTSTRAP_SERVER);
+		props.put("group.id", GROUP_ID);
+		props.put("enable.auto.commit", ENABLE_AUTO_COMMIT);
+		props.put("auto.offset.reset", AUTO_OFFSET_RESET);
+		props.put("key.deserializer", KEY_DESERIALIZER);
+		props.put("value.deserializer", VALUE_DESERIALIZER);
+
 		int partitionNum = Integer.parseInt(args[0]);
-		
+
 		long executeTime = System.currentTimeMillis();
 		long executable = executeTime + 10000;
 
-		//Create Kafka Consumer
+		// Create Kafka Consumer
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-		
-		//Connect partition to Kafka Consumer
-		TopicPartition partition = new TopicPartition("idpl-topic", partitionNum);
+
+		// Connect partition to Kafka Consumer
+		TopicPartition partition = new TopicPartition(TOPIC_NAME, partitionNum);
 		consumer.assign(Arrays.asList(partition));
 		consumer.seek(partition, 0);
-		
-		long consumerStartTime = System.currentTimeMillis();
+
+		PrintWriter pw = new PrintWriter(RESULT_FILE_NAME + args[0]);
+
+		pw.println("Consumer-" + args[0] + " Start Time : " + (System.currentTimeMillis() / 1000));
+
 		try {
 			while (executeTime < executable) {
-				ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10000));
-				
-				//Parse String to Long and sleep Thread
+				ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+				// Parse String to Long and sleep Thread
 				for (ConsumerRecord<String, String> record : records) {
 					System.out.printf("Value : %s, Offset : %d\n", record.value(), record.offset());
 					Thread.sleep(Long.parseLong(record.value()) * 1000);
 					executable = System.currentTimeMillis() + 10000;
 				}
-				//Wait for not arrived task in 10sec
+				// Wait for not arrived task in 10sec
 				executeTime = System.currentTimeMillis();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			//Text file : File for check Consumer's running time.
-			PrintWriter pw = new PrintWriter("ConsumerResult-" + args[0]);
-			pw.println("Consumer" + args[0] + " Execution Time : " + 
-						((System.currentTimeMillis() - consumerStartTime)/1000));
+			// Text file : File for check Consumer's running time.
+			pw.println("Consumer-" + args[0] + " End Time : " + (System.currentTimeMillis() / 1000));
 			pw.close();
 			consumer.close();
 			System.out.println("Consumer is Closed..");
